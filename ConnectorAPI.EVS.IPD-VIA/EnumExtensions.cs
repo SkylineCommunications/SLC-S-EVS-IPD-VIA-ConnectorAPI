ae1ec0364dd0f57e559267d348992a2bef2f83e8
VictorScherpereell <victor.scherpereel@skyline.be>
2025-01-27 11:06:34 +0100

    Added enumextensions

---
+++ ConnectorAPI.EVS.IPD-VIA/EnumExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace Skyline.DataMiner.ConnectorAPI.EVS.IPD_VIA
+{
+	using System;
+	using System.ComponentModel;
+	using System.Linq;
+
+	/// <summary>
+	/// A extension class for enums.
+	/// </summary>
+	public static class EnumExtensions
+	{
+		/// <summary>
+		/// Gets the value of the <see cref="DescriptionAttribute"/> on the enum value if any. Otherwise it returns the enum value as string.
+		/// </summary>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static string GetDescription(this Enum value)
+		{
+			var attribute = value.GetType().GetField(value.ToString())
+				.GetCustomAttributes(typeof(DescriptionAttribute), false)
+				.SingleOrDefault() as DescriptionAttribute;
+
+			return attribute is null
+				? value.ToString()
+				: attribute.Description;
+		}
+	}
+}
